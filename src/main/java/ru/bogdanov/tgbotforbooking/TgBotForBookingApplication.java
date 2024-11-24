fda1399fc40b7902a93ea5a3a7f988194b240154
Mikhail Bogdanov <bogdanov28031996@gmail.com>
2024-11-24 18:31:47 +0300

    init commit

---
+++ src/main/java/ru/bogdanov/tgbotforbooking/TgBotForBookingApplication.java
@@ -0,0 +1,13 @@
+package ru.bogdanov.tgbotforbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotForBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotForBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bogdanov/tgbotforbooking/TgBotForBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bogdanov.tgbotforbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotForBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
