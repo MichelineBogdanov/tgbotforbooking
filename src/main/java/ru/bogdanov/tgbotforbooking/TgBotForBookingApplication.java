d5afb86a00be601b4f6c3931842e21d40c1d8f34
Mikhail Bogdanov <bogdanov28031996@gmail.com>
2025-01-15 00:27:27 +0300

    Spring Actuator Demo [+] - quick start

--- src/main/java/ru/bogdanov/tgbotforbooking/TgBotForBookingApplication.java
+++ src/main/java/ru/bogdanov/tgbotforbooking/TgBotForBookingApplication.java
@@ -1,9 +1,11 @@
 package ru.bogdanov.tgbotforbooking;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class TgBotForBookingApplication {
 
 	public static void main(String[] args) {
