31bda0b5796a0590ef3730654cbb6dc719ef97d2
Mikhail Bogdanov <100092982+MichelineBogdanov@users.noreply.github.com>
2025-07-29 15:57:36 +0300

    Refactoring
    [!] - extract DateTimeFormatter to avoid of creating new instances

--- src/main/java/ru/bogdanov/tgbotforbooking/services/telegram/utils/DateTimeUtils.java
+++ src/main/java/ru/bogdanov/tgbotforbooking/services/telegram/utils/DateTimeUtils.java
@@ -9,24 +9,28 @@ import java.util.Date;
 
 public class DateTimeUtils {
 
-    private static final String DATE_TIME_PATTERN = "dd-MM-yyyy HH:mm";
-    private static final String DATE_PATTERN = "dd-MM-yyyy";
-    private static final String DAY_PATTERN = "dd.MM";
+    private static final String DATE_TIME_STRING_PATTERN = "dd-MM-yyyy HH:mm";
+    private static final String DATE_STRING_PATTERN = "dd-MM-yyyy";
+    private static final String DAY_STRING_PATTERN = "dd.MM";
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_STRING_PATTERN);
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_STRING_PATTERN);
+    private static final DateTimeFormatter DAY_FORMATTER = DateTimeFormatter.ofPattern(DAY_STRING_PATTERN);
 
     public static Date fromLocalDateTimeToDate(LocalDateTime localDateTime) {
         return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 
     public static String fromLocalDateTimeToDateTimeString(LocalDateTime localDateTime) {
-        return localDateTime.format(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN));
+        return localDateTime.format(DATE_TIME_FORMATTER);
     }
 
     public static String fromLocalDateToDateString(LocalDate localDate) {
-        return localDate.format(DateTimeFormatter.ofPattern(DATE_PATTERN));
+        return localDate.format(DATE_FORMATTER);
     }
 
     public static String fromLocalDateToDayString(LocalDate localDate) {
-        return localDate.format(DateTimeFormatter.ofPattern(DAY_PATTERN));
+        return localDate.format(DAY_FORMATTER);
     }
 
     public static LocalDateTime parseDateTimeFromRFC3339(String rfc3339DateStr) {
